d074ff033b80ff7a4ac13e166c6353db5963427c
David Alim <daveay@gmail.com>
2018-09-26 04:53:45 -0400

    db url fix??

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'ds133570.mlab.com:33570/mongoose-blog-api';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://DFA:password1@ds133570.mlab.com:33570/mongoose-blog-api';
 exports.PORT = process.env.PORT || 8080;
 
